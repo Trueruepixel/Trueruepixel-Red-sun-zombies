dfd1477a919051722caf8cdc91689f31ac3299fe
Trueruepixel <99rpg99@gmail.com>
2026-07-25 22:44:21 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='red-sun-v2';
-const ASSETS=['./','index.html','manifest.webmanifest',
-'assets/argentum.png','assets/forgotten.png','assets/patches.png','assets/ken.png','assets/preem.png',
-'assets/pressure-rising.wav','assets/icon-192.png','assets/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
